bba096a39ca12c0b54338184adc977b04c4787c7
SeniorDesign1SnowIoT <tellesf1@hawkmail.newpaltz.edu>
2017-02-20 00:33:22 -0500

    First commit

---
+++ app/src/main/java/com/example/snowiot/snowiotsimple/Login.java
@@ -0,0 +1,109 @@
+//Code obtained from TVAC studio video tutorial on youtube on how to create user authentication for firebase
+
+package com.example.snowiot.snowiotsimple;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    //to check if user is already or still logged in
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private Button mLoginButton;
+
+    //define firebase auth
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmailField = (EditText) findViewById(R.id.emailField);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            if(firebaseAuth.getCurrentUser() != null) {             //If getCurrentUser == null, user has not logged in. if != null, user has logged in.
+
+                startActivity(new Intent(Login.this, MainActivity.class)); //if user is logged in, start new intent from Login activity to MainActivity.
+
+            }
+
+            }
+        };
+
+        //as told by firebase docs
+        mAuth = FirebaseAuth.getInstance();
+
+        //Event listener for login button
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                startSignin();
+            }
+        });
+
+    }
+
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+
+
+
+
+//Custom function for signing
+private void startSignin(){
+    String email = mEmailField.getText().toString();
+    String password = mPasswordField.getText().toString();
+
+    //if email and password fields are empty, then solve without a bug
+    if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password))
+    {
+        Toast.makeText(Login.this, "Information fields are blank.", Toast.LENGTH_LONG).show();
+    }
+
+    else {
+
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (!task.isSuccessful()) {
+                    Toast.makeText(Login.this, "Invalid username or password.", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+    }
+}
+
+}
--- app/src/main/java/com/example/snowiot/snowiotsimple/MainActivity.java
+++ app/src/main/java/com/example/snowiot/snowiotsimple/MainActivity.java
@@ -1,29 +1,37 @@
 package com.example.snowiot.snowiotsimple;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.view.MenuItem;
 import android.view.ViewDebug;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Switch mSetLedSignal;
     ListView mListView;
+    ImageView snowIotTitle;
 
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
@@ -37,11 +45,14 @@ public class MainActivity extends AppCompatActivity {
 
             mListView = (ListView) findViewById(R.id.sensorListView);
             mSetLedSignal = (Switch) findViewById(R.id.ledSignal);
+            snowIotTitle = (ImageView) findViewById(R.id.snowIotText);
+
+            Picasso.with(getApplicationContext()).load("http://i.imgur.com/qoku2bl.png").into(snowIotTitle); //pass image into imgview
 
             DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://snowtotals-68015.firebaseio.com/Sensor1");
             //DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
-            FirebaseListAdapter mAdapter = new FirebaseListAdapter<Sensors>(this, Sensors.class, android.R.layout.two_line_list_item, databaseReference) {
+            FirebaseListAdapter mAdapter = new FirebaseListAdapter<Sensors>(this, Sensors.class, android.R.layout.two_line_list_item, databaseReference) {          //Listview using Sensors class
                 @Override
                 protected void populateView(View view, Sensors chatMessage, int position) {
                     ((TextView)view.findViewById(android.R.id.text1)).setText(chatMessage.getName());
@@ -59,13 +70,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        //LED Switch block
+        //LED Switch code block
         mSetLedSignal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean switchPressed) {
 
                 if (switchPressed) {
-                    Toast.makeText(getApplicationContext(), "Sensors ON.", Toast.LENGTH_SHORT).show(); //Display message on screen.
+                    Toast.makeText(getApplicationContext(), "Sensors ON", Toast.LENGTH_SHORT).show(); //Display message on screen.
                     mLedSignalRef.setValue(1);  //if switch is checked, then set LED signal to 1 (on firebase)
                 } else {
                     Toast.makeText(getApplicationContext(), "Sensors OFF", Toast.LENGTH_SHORT).show();
@@ -77,6 +88,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+//Code by Andres Menendez (Youtube)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        if (id == R.id.action_blog) {
+            Intent blogLink = new Intent(Intent.ACTION_VIEW, Uri.parse("https://snow-sense.github.io/IoTSnow/"));
+            startActivity(blogLink);
+            return true;
         }
+
+        if (id == R.id.action_credits) {
+            Intent creditsSelect = new Intent(MainActivity.this, Credits.class);
+            startActivity(creditsSelect);
+            return true;
+        }
+
+        if (id == R.id.action_signout) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+//Back button won't close MainActivity
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(false);
+    }
+}
